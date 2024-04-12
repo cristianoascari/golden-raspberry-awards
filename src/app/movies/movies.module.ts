7429fdc376d97cfd9be8250f775e7595e494c32f
Cristiano Ascari <cristiano.ascari@gmail.com>
2024-04-12 13:56:20 -0300

    Dashboard and movies modules created.

---
+++ src/app/movies/movies.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { MoviesComponent } from './movies.component';
+
+@NgModule({
+  declarations: [
+    MoviesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MoviesModule { }
